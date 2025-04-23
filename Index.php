2b9be0e7666ce2d15f1516e7aed2f3e28ea4305f
Kauã Silva <kauas657@gmail.com>
2025-04-23 15:39:08 -0300

    form resolvido

--- Index.php
+++ Index.php
@@ -5,12 +5,12 @@ $senha="";
 $db="todolist";
 
 $conexao=mysqli_connect($servidor,$usuario,$senha,$db);
-$query="SELECT *FROM LISTA";
+$query="SELECT * FROM LISTA";
 $resultado=mysqli_query($conexao,$query);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
@@ -18,52 +18,50 @@ $resultado=mysqli_query($conexao,$query);
     <title>Adicionar Tarefa | Listify</title>
     <link rel="stylesheet" href="./style.css" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/2554b6ec93.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <div class="Menu">
-        <div class="LogoListify">
-            <img src="./imgs/LogoListifySemFundo.png" alt="" width=213px height=135px>
+    <div class="divzona">
+        <div class="Menu">
+            <div class="LogoListify">
+                <img src="./imgs/LogoListifySemFundo.png" alt="Logo Listify" width="213" height="135">
+            </div>
+            <div class="btnsMenu">
+                <div class="btnHome">
+                    <a href="?pagina=form" class="ahome">
+                        <i class="fa-solid fa-house casa"></i>
+                        <button class="home">Home</button>
+                    </a>
+                </div>
+                <div class="btnMensal">
+                    <a href="#" class="amensal">
+                        <i class="fa-regular fa-calendar mensalicon"></i>
+                        <button class="mensal">Mensal</button>
+                    </a>
+                </div>
+                <div class="linha"></div>
+                <div class="btnAdicionar">
+                    <a href="#" class="aadicionar">
+                        <i class="fa-solid fa-plus adicionaricon"></i>
+                        <button class="adicionar">Adicionar</button>
+                    </a>
+                </div>
+            </div>
         </div>
-        <div class="btnsMenu">
-            <div class="btnHome">
-                <a href="?pagina=form" class="ahome">
-                    <i class="fa-solid fa-house casa"></i>
-                    <button class="home">Home</button>
-                </a>
-            </div>
-            <div class="btnMensal">
-                <a href="" class="amensal">
-                <i class="fa-regular fa-calendar mensalicon"></i>
-                    <button class="mensal">Mensal</button>
-                </a>
-            </div>
-            <div class="linha"></div>
-            <div class="btnAdicionar">
-                <a href="" class="aadicionar">
-                <i class="fa-solid fa-plus adicionaricon"></i>
-                    <button class="adicionar">Adicionar</button>
-                </a>
+
+        <div class="formholder">
+            <div class="Form">
+                <form action="bd.php" method="POST">
+                    <input class="form-control" type="text" placeholder="Nome da Tarefa" name="tarefa">
+                    <input class="form-control" type="text" placeholder="Descrição" name="descricao">
+                    <input class="form-control" type="date" name="dataInicio">
+                    <input class="form-control" type="date" name="dataFim">
+                    <button type="submit" class="btn btn-dark">Criar Tarefa</button>
+                </form>
             </div>
         </div>
     </div>
-    <div class="Title">
-        <h1>Adicionar Nova Tarefa</h1>
-    </div>
-    <div class="Form">
-        <form action="bd.php" method="POST">
-            <input class="form-control" type="text" placeholder="Default input" aria-label="default input example"
-                name="tarefa">
-            <input class="form-control" type="text" placeholder="Default input" aria-label="default input example"
-                name="descricao">
-            <input class="form-control" type="date" placeholder="Default input" aria-label="default input example"
-                name="dataInicio">
-            <input class="form-control" type="date" placeholder="Default input" aria-label="default input example"
-                name="dataFim">
-            <button type="submit" value="inserir afazer" class="btn btn-dark">Criar Tarefa</button>
-        </form>
-    </div>
-    <script src="https://kit.fontawesome.com/2554b6ec93.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
