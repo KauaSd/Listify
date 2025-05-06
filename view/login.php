8fbb6d78c773f6fb01bfc231d99b456ed4db023a
lerkive <leticiaoliveirasilva898@gmail.com>
2025-05-05 21:59:31 -0300

    detalhes do login e bolinha

--- view/login.php
+++ view/login.php
@@ -1,29 +1,32 @@
-<div class="divzonalogin">
-<div class="Header">
-	<div class="FundoHeader">
-		<div class="ListifyImg">
-		<img src="imgs/listify branco.png" class="imgheader" alt="">
+<body class="loginPage">
+	<div class="divzonalogin">
+		<div class="Header">
+			<div class="FundoHeader">
+				<div class="ListifyImg">
+					<img src="imgs/listify branco.png" class="imgheader" alt="">
+				</div>
+			</div>
 		</div>
+		<div class="formlogin">
+			<div class="quadradologin">
+				<h1>Entre em sua conta</h1>
+				<br>
+				<form method="post" action="verificacao.php">
+					<label>Digite o nome do usuário</label><br>
+					<input type="text" name="usuario" placeholder="Nome do usuário">
+					<br><br>
+					<label>Digite a senha</label><br>
+					<input type="password" name="senha" placeholder="Senha">
+					<br><br>
+					<input type="submit" value="Entrar" class="btnentrar">
+				</form>
+			</div>
+		</div>
+		<br>
 	</div>
-</div>
-<div class="formlogin">
-	<div class="quadradologin">
-	<h1>Entre na sua conta</h1>
-<form method="post" action="verificacao.php">
-	<label>Digite o nome do usuário</label><br>
-	<input type="text" name="usuario" placeholder="Nome do usuário">
-	<br><br>
-	<label>Digite a senha</label><br>
-	<input type="password" name="senha" placeholder="Senha">
-	<br><br>
-	<input type="submit" value="Entrar" class="btnentrar">
-</form>
-</div>
-</div>
-<br>
-</div>
-<?php if (isset($_GET['erro'])){ ?>
-	<div class="alert" role="alert">
-		Usuário e/ou senha inválido(s).
-	</div>
-<?php } ?>
+	<?php if (isset($_GET['erro'])){ ?>
+		<div class="alert" role="alert">
+			Usuário e/ou senha inválido(s).
+		</div>
+	<?php } ?>
+</body>
